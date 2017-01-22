50915c5e00536a2276b9da7c8796730f466a85ac
Oscar Rieken <oscar.rieken@adp.com>
2017-01-21 23:34:15 -0500

    :book: change app name to StoryVent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,16 +19,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'StoryVent'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('StoryVent');
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('StoryVent');
   }));
+
 });
